381d71fd8ac1cdad410f9dd4305b62094f852c2d
Gal Elmalah <gale@wix.com>
2020-07-11 16:47:25 +0300

    update hpmepage

---
+++ build/precache-manifest.8e98cb3718dbedde3623cfb544357fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d592477451b30f8d8d1d3f13e12e0860",
+    "url": "/lost-and-found/index.html"
+  },
+  {
+    "revision": "1d006ec0b40bb65a150f",
+    "url": "/lost-and-found/static/css/main.ebb43620.chunk.css"
+  },
+  {
+    "revision": "f1de579d75c671fdae6b",
+    "url": "/lost-and-found/static/js/2.8bcb9ea1.chunk.js"
+  },
+  {
+    "revision": "cdfe19c4b4620d234f5a200884f251cd",
+    "url": "/lost-and-found/static/js/2.8bcb9ea1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d006ec0b40bb65a150f",
+    "url": "/lost-and-found/static/js/main.2c144255.chunk.js"
+  },
+  {
+    "revision": "14b3561be169c25b4600",
+    "url": "/lost-and-found/static/js/runtime-main.173b932f.js"
+  }
+]);
